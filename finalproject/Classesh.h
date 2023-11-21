004d40145eb8f8ca75e7434a8ded716584feea09
Syed Ali Hassan Zaidi <108427686+zaidiali9@users.noreply.github.com>
2023-11-21 22:10:15 +0500

    Attributes added

--- finalproject/Classesh.h
+++ finalproject/Classesh.h
@@ -1,16 +1,23 @@
 #include <iostream>
 #include <vector>
 class System {
+	vector<Person> persons;
+	vector<Complaient> complaients;
 
 };
 class Person {
+	int pID;
+	string name;
+	string email;
+	string address;
 
 };
 class Employee : public Person {
-
+	Department *department;
+	Manager *manager;
 };
 class Manager : public Person {
-
+	Job *jobs[];
 };
 class Teacher : public Person {
 
@@ -20,10 +27,17 @@ class Job {
 };
 
 class Department {
-
+	int ID;
+	string name;
 };
 class Complaient {
 	ComplaientStatus *status;
+	string description;
+	string date;
+	string time;
+	int compid;
+	string feedback;
+	int pID;
 };
 class ComplaientStatus {
 	
